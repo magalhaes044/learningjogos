abb791ca483643d81a6f01f9533f0cea63fb515c
magalhaes044 <129965153+magalhaes044@users.noreply.github.com>
2023-06-14 01:08:36 -0300

    teste5

--- script.js
+++ script.js
@@ -1,209 +1,183 @@
-var grid = [];
-var currentShape;
-var currentShapeCoords;
-var interval;
-var score = 0;
-var isGameRunning = false;
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.getElementById('grid');
+    const scoreDisplay = document.getElementById('score');
+    const startButton = document.getElementById('start-button');
 
-function createGrid() {
-  for (var row = 0; row < 20; row++) {
-    grid[row] = [];
-    for (var col = 0; col < 10; col++) {
-      grid[row][col] = 0;
+    const gridWidth = 10;
+    const gridHeight = 20;
+    const cellSize = 30;
+    const emptyCell = '';
+
+    let gridCells = [];
+    let currentTetromino = null;
+    let timerId = null;
+    let score = 0;
+
+    const tetrominos = [
+        [
+            [1, 1, 1, 1], // I
+        ],
+        [
+            [1, 1], // O
+            [1, 1],
+        ],
+        [
+            [1, 1, 1], // T
+            [0, 1, 0],
+        ],
+        [
+            [1, 1, 1], // L
+            [1, 0, 0],
+        ],
+        [
+            [1, 1, 0], // J
+            [0, 0, 1],
+        ],
+        [
+            [0, 1, 1], // S
+            [1, 1, 0],
+        ],
+        [
+            [1, 1, 1], // Z
+            [0, 1, 1],
+        ],
+    ];
+
+    const tetrominoColors = [
+        '#00FFFF', // Cyan
+        '#FFFF00', // Yellow
+        '#FF00FF', // Magenta
+        '#00FF00', // Green
+        '#0000FF', // Blue
+        '#FFA500', // Orange
+        '#FF0000', // Red
+    ];
+
+    function createGrid() {
+        for (let i = 0; i < gridHeight; i++) {
+            const row = [];
+            for (let j = 0; j < gridWidth; j++) {
+                const cell = document.createElement('div');
+                cell.classList.add('cell');
+                cell.style.width = `${cellSize}px`;
+                cell.style.height = `${cellSize}px`;
+                row.push(cell);
+                grid.appendChild(cell);
+            }
+            gridCells.push(row);
+        }
     }
-  }
-}
 
-function drawGrid() {
-  var tetrisGrid = document.getElementById('tetris-grid');
-  tetrisGrid.innerHTML = '';
-
-  for (var row = 0; row < 20; row++) {
-    for (var col = 0; col < 10; col++) {
-      var cell = document.createElement('div');
-      cell.className = grid[row][col] === 0 ? 'cell' : 'cell active';
-      tetrisGrid.appendChild(cell);
+    function drawGrid() {
+        for (let i = 0; i < gridHeight; i++) {
+            for (let j = 0; j < gridWidth; j++) {
+                const cell = gridCells[i][j];
+                cell.style.backgroundColor = grid[i][j] || emptyCell;
+            }
+        }
     }
-  }
-}
 
-function createShape() {
-  var shapes = [
-    [[1, 1, 1, 1]], // I
-    [[1, 1], [1, 1]], // O
-    [[1, 1, 1], [0, 1, 0]], // T
-    [[1, 1, 0], [0, 1, 1]], // S
-    [[0, 1, 1], [1, 1, 0]], // Z
-    [[1, 1, 1], [1, 0, 0]], // J
-    [[1, 1, 1], [0, 0, 1]] // L
-  ];
-
-  var randomShape = shapes[Math.floor(Math.random() * shapes.length)];
-  currentShape = randomShape;
-  currentShapeCoords = {
-    x: 4,
-    y: 0
-  };
-}
-
-function drawShape() {
-  var tetrisGrid = document.getElementById('tetris-grid');
-
-  for (var row = 0; row < currentShape.length; row++) {
-    for (var col = 0; col < currentShape[row].length; col++) {
-      if (currentShape[row][col]) {
-        var cell = document.createElement('div');
-        cell.className = 'shape';
-        cell.style.left = (currentShapeCoords.x + col) * 20 + 'px';
-        cell.style.top = (currentShapeCoords.y + row) * 20 + 'px';
-        tetrisGrid.appendChild(cell);
-      }
+    function rotateTetromino() {
+        const rotatedTetromino = [];
+        const rows = currentTetromino[0].length;
+        const cols = currentTetromino.length;
+        for (let i = 0; i < rows; i++) {
+            const newRow = [];
+            for (let j = 0; j < cols; j++) {
+                newRow.push(currentTetromino[j][i]);
+            }
+            rotatedTetromino.push(newRow.reverse());
+        }
+        return rotatedTetromino;
     }
-  }
-}
 
-function removeShape() {
-  var tetrisGrid = document.getElementById('tetris-grid');
-  var shapes = tetrisGrid.getElementsByClassName('shape');
-
-  while (shapes.length > 0) {
-    shapes[0].parentNode.removeChild(shapes[0]);
-  }
-}
-
-function isCollision() {
-  for (var row = 0; row < currentShape.length; row++) {
-    for (var col = 0; col < currentShape[row].length; col++) {
-      if (
-        currentShape[row][col] &&
-        (currentShapeCoords.y + row >= 20 ||
-          currentShapeCoords.x + col < 0 ||
-          currentShapeCoords.x + col >= 10 ||
-          grid[currentShapeCoords.y + row][currentShapeCoords.x + col])
-      ) {
-        return true;
-      }
+    function isCollision() {
+        const tetromino = currentTetromino;
+        const rowOffset = tetromino.length;
+        const colOffset = tetromino[0].length;
+        for (let i = 0; i < tetromino.length; i++) {
+            for (let j = 0; j < tetromino[0].length; j++) {
+                if (tetromino[i][j] && (grid[i + rowOffset] && grid[i + rowOffset][j + colOffset]) !== emptyCell) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
-  }
 
-  return false;
-}
-
-function mergeShape() {
-  for (var row = 0; row < currentShape.length; row++) {
-    for (var col = 0; col < currentShape[row].length; col++) {
-      if (currentShape[row][col]) {
-        grid[currentShapeCoords.y + row][currentShapeCoords.x + col] = 1;
-      }
+    function mergeTetromino() {
+        const tetromino = currentTetromino;
+        const rowOffset = tetromino.length;
+        const colOffset = tetromino[0].length;
+        for (let i = 0; i < tetromino.length; i++) {
+            for (let j = 0; j < tetromino[0].length; j++) {
+                if (tetromino[i][j]) {
+                    grid[i + rowOffset][j + colOffset] = tetrominoColors.indexOf(currentTetrominoColor);
+                }
+            }
+        }
+        removeFullRows();
     }
-  }
-}
 
-function checkRows() {
-  for (var row = 19; row >= 0; row--) {
-    if (grid[row].every((cell) => cell)) {
-      score++;
-      grid.splice(row, 1);
-      grid.unshift(new Array(10).fill(0));
+    function removeFullRows() {
+        let rowsCleared = 0;
+        for (let i = gridHeight - 1; i >= 0; i--) {
+            if (grid[i].every(cell => cell !== emptyCell)) {
+                grid.splice(i, 1);
+                grid.unshift(Array(gridWidth).fill(emptyCell));
+                rowsCleared++;
+            }
+        }
+        if (rowsCleared > 0) {
+            score += rowsCleared * 10;
+            scoreDisplay.textContent = score;
+        }
     }
-  }
-}
 
-function updateScore() {
-  document.getElementById('score').innerHTML = 'Score: ' + score;
-}
-
-function moveDown() {
-  removeShape();
-  currentShapeCoords.y++;
-
-  if (isCollision()) {
-    currentShapeCoords.y--;
-    mergeShape();
-    createShape();
-
-    if (isCollision()) {
-      clearInterval(interval);
-      isGameRunning = false;
-      alert('Game Over!');
-      return;
+    function update() {
+        if (!isCollision()) {
+            currentTetrominoRow++;
+        } else {
+            mergeTetromino();
+            if (currentTetrominoRow < 0) {
+                // Game Over
+                clearInterval(timerId);
+                alert('Game Over');
+                return;
+            }
+            currentTetromino = getRandomTetromino();
+            currentTetrominoColor = getRandomTetrominoColor();
+            currentTetrominoRow = -1;
+            currentTetrominoCol = Math.floor(gridWidth / 2) - Math.floor(currentTetromino[0].length / 2);
+        }
+        drawGrid();
     }
-  }
 
-  drawShape();
-}
-
-function moveLeft() {
-  removeShape();
-  currentShapeCoords.x--;
-
-  if (isCollision()) {
-    currentShapeCoords.x++;
-  }
-
-  drawShape();
-}
-
-function moveRight() {
-  removeShape();
-  currentShapeCoords.x++;
-
-  if (isCollision()) {
-    currentShapeCoords.x--;
-  }
-
-  drawShape();
-}
-
-function rotateShape() {
-  removeShape();
-
-  var rotatedShape = [];
-  for (var col = 0; col < currentShape[0].length; col++) {
-    var newRow = [];
-    for (var row = currentShape.length - 1; row >= 0; row--) {
-      newRow.push(currentShape[row][col]);
+    function getRandomTetromino() {
+        const randomIndex = Math.floor(Math.random() * tetrominos.length);
+        return tetrominos[randomIndex];
     }
-    rotatedShape.push(newRow);
-  }
 
-  currentShape = rotatedShape;
-
-  if (isCollision()) {
-    rotateShape();
-  }
-
-  drawShape();
-}
-
-document.addEventListener('keydown', function (event) {
-  if (isGameRunning) {
-    switch (event.key) {
-      case 'ArrowDown':
-        moveDown();
-        break;
-      case 'ArrowLeft':
-        moveLeft();
-        break;
-      case 'ArrowRight':
-        moveRight();
-        break;
-      case 'ArrowUp':
-        rotateShape();
-        break;
+    function getRandomTetrominoColor() {
+        const randomIndex = Math.floor(Math.random() * tetrominoColors.length);
+        return tetrominoColors[randomIndex];
     }
-  }
-});
 
-document.getElementById('start-button').addEventListener('click', function () {
-  if (!isGameRunning) {
-    isGameRunning = true;
-    score = 0;
+    function startGame() {
+        gridCells = [];
+        grid = Array(gridHeight).fill().map(() => Array(gridWidth).fill(emptyCell));
+        drawGrid();
+        score = 0;
+        scoreDisplay.textContent = score;
+        currentTetromino = getRandomTetromino();
+        currentTetrominoColor = getRandomTetrominoColor();
+        currentTetrominoRow = -1;
+        currentTetrominoCol = Math.floor(gridWidth / 2) - Math.floor(currentTetromino[0].length / 2);
+        timerId = setInterval(update, 1000);
+        startButton.disabled = true;
+    }
+
     createGrid();
-    drawGrid();
-    createShape();
-    drawShape();
-    updateScore();
-    interval = setInterval(moveDown, 500);
-  }
+
+    startButton.addEventListener('click', startGame);
 });
